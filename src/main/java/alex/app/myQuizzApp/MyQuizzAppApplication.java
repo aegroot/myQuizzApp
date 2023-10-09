2cb5037a3ff1c0f3cfa8d9bfe66bbbf73a0fe6a5
aegroot <alex-groot@live.nl>
2023-10-09 13:50:23 +0200

    init

---
+++ src/main/java/alex/app/myQuizzApp/MyQuizzAppApplication.java
@@ -0,0 +1,13 @@
+package alex.app.myQuizzApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyQuizzAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyQuizzAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/alex/app/myQuizzApp/MyQuizzAppApplicationTests.java
@@ -0,0 +1,13 @@
+package alex.app.myQuizzApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyQuizzAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

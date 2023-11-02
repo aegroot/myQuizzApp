257ddb1ae25d1d2d4445b180285b085ec899f1ec
aegroot <alex-groot@live.nl>
2023-11-02 15:44:24 +0100

    added reusable components added service classes

---
+++ frontend/frontend/src/components/makequiz/QuizResults.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function QuizResults() {
+  return (
+    <div>QuizResults
+      <div>quiz data</div>
+      <div>quiz score</div>
+      <div>question summary table</div>
+      <button>to homepage</button>
+
+    </div>
+
+    
+  )
+}
